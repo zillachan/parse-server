ea33ef689de696eb5dc6bbdd7a4398cb771b4611
Florent Vilmart <florentvilmart@me.com>
2016-07-18 17:05:28 -0400

    attempt to fix flaky test

--- spec/CloudCodeLogger.spec.js
+++ spec/CloudCodeLogger.spec.js
@@ -3,32 +3,6 @@ var LoggerController = require('../src/Controllers/LoggerController').LoggerCont
 var FileLoggerAdapter = require('../src/Adapters/Logger/FileLoggerAdapter').FileLoggerAdapter;
 
 describe("Cloud Code Logger", () => {
-    it("should expose log to functions", (done) => {
-        var logController = new LoggerController(new FileLoggerAdapter());
-
-        Parse.Cloud.define("loggerTest", (req, res) => {
-            req.log.info('logTest', 'info log', {info: 'some log' });
-            req.log.error('logTest','error log', {error: 'there was an error'});
-            res.success({});
-        });
-
-        Parse.Cloud.run('loggerTest').then(() => {
-            return logController.getLogs({from: Date.now() - 500, size: 1000});
-        }).then((res) => {
-            expect(res.length).not.toBe(0);
-            let lastLogs =  res.slice(0, 2);
-            let errorMessage = lastLogs[0];
-            let infoMessage = lastLogs[1];
-            expect(errorMessage.level).toBe('error');
-            expect(errorMessage.error).toBe('there was an error');
-            expect(errorMessage.message).toBe('logTest error log');
-            expect(infoMessage.level).toBe('info');
-            expect(infoMessage.info).toBe('some log');
-            expect(infoMessage.message).toBe('logTest info log');
-            done();
-        });
-    });
-
     it("should expose log to trigger", (done) => {
         var logController = new LoggerController(new FileLoggerAdapter());
 
@@ -52,6 +26,32 @@ describe("Cloud Code Logger", () => {
             expect(infoMessage.level).toBe('info');
             expect(infoMessage.info).toBe('some log');
             expect(infoMessage.message).toBe('beforeSave MyObject info log');
+            done();
+        });
+    });
+
+    it("should expose log to functions", (done) => {
+        var logController = new LoggerController(new FileLoggerAdapter());
+
+        Parse.Cloud.define("loggerTest", (req, res) => {
+            req.log.info('logTest', 'info log', {info: 'some log' });
+            req.log.error('logTest','error log', {error: 'there was an error'});
+            res.success({});
+        });
+
+        Parse.Cloud.run('loggerTest').then(() => {
+            return logController.getLogs({from: Date.now() - 500, size: 1000});
+        }).then((res) => {
+            expect(res.length).not.toBe(0);
+            let lastLogs =  res.slice(0, 2);
+            let errorMessage = lastLogs[0];
+            let infoMessage = lastLogs[1];
+            expect(errorMessage.level).toBe('error');
+            expect(errorMessage.error).toBe('there was an error');
+            expect(errorMessage.message).toBe('logTest error log');
+            expect(infoMessage.level).toBe('info');
+            expect(infoMessage.info).toBe('some log');
+            expect(infoMessage.message).toBe('logTest info log');
             done();
         });
     });
